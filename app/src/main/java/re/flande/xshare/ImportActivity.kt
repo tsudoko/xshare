60e08bc19a6d36a78719a0920c7f66eaf0378e4a
flan <flan@flande.re>
2017-07-22 16:33:11 +0200

    Implement importing configs

---
+++ app/src/main/java/re/flande/xshare/ImportActivity.kt
@@ -0,0 +1,24 @@
+package re.flande.xshare
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Context
+import android.os.Bundle
+
+class ImportActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val pd = ProgressDialog.show(this, "", resources.getString(R.string.importing))
+        contentResolver.openInputStream(intent.data).use { in_ ->
+            val name = intent.data.path.split("/").last()
+            openFileOutput(name, Context.MODE_PRIVATE).use { out ->
+                Util.copy(in_, out)
+            }
+        }
+        // TODO: config validation
+        pd.cancel()
+        finishAffinity()
+    }
+}
