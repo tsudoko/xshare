1335b5012ee35332d5806863058b1ae4e48ffc00
flan <flan@flande.re>
2017-07-22 14:19:10 +0200

    Don't crash when the config doesn't exist

--- app/src/main/java/re/flande/xshare/Uploader.kt
+++ app/src/main/java/re/flande/xshare/Uploader.kt
@@ -10,9 +10,11 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.Log
+import android.widget.Toast
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Method
 import com.google.gson.Gson
+import java.io.FileNotFoundException
 import java.io.FileOutputStream
 
 class Uploader : Activity() {
@@ -43,71 +45,75 @@ class Uploader : Activity() {
             }
         }
 
-        val f = openFileInput("$uploader.sxcu")
-        val config = Gson().fromJson(f.reader(), Config::class.java)
+        try {
+            openFileInput("$uploader.sxcu").use { f ->
+                val config = Gson().fromJson(f.reader(), Config::class.java)
 
-        var rurl = config?.RequestURL ?: throw Exception("no uploader url specified")
-        if(!rurl.startsWith("http"))
-            rurl = "http://" + rurl
+                var rurl = config?.RequestURL ?: throw Exception("no uploader url specified")
+                if (!rurl.startsWith("http"))
+                    rurl = "http://" + rurl
 
-        val nBuilder = Notification.Builder(this)
-                .setContentTitle("Uploading $path...")
-                .setProgress(100, 0, true)
-                .setOngoing(true)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                val nBuilder = Notification.Builder(this)
+                        .setContentTitle("Uploading $path...")
+                        .setProgress(100, 0, true)
+                        .setOngoing(true)
+                        .setSmallIcon(R.mipmap.ic_launcher)
 
-        notifManager.notify(notifID, nBuilder.build())
+                notifManager.notify(notifID, nBuilder.build())
 
-        var time = SystemClock.uptimeMillis()
+                var time = SystemClock.uptimeMillis()
 
-        Fuel.upload(rurl, Method.valueOf(config.RequestType ?: "POST"), config.Arguments?.toList())
-                .header(config.Headers)
-                .source { req, url ->
-                    req.name = config.FileFormName
-                    tempFile
-                }
-                .progress { read, total ->
-                    //Log.d(TAG, "read $read total $total")
-                    // .progress gets called once with read=$total, total=0 before resolving the hostname for reasons unknown to me
-                    if(total == 0L)
-                        return@progress
+                Fuel.upload(rurl, Method.valueOf(config.RequestType ?: "POST"), config.Arguments?.toList())
+                        .header(config.Headers)
+                        .source { req, url ->
+                            req.name = config.FileFormName
+                            tempFile
+                        }
+                        .progress { read, total ->
+                            //Log.d(TAG, "read $read total $total")
+                            // .progress gets called once with read=$total, total=0 before resolving the hostname for reasons unknown to me
+                            if (total == 0L)
+                                return@progress
 
-                    // avoid slowing down the system with excessive notifications
-                    val curTime = SystemClock.uptimeMillis()
-                    if(curTime - time < 100)
-                        return@progress
-                    else
-                        time = curTime
+                            // avoid slowing down the system with excessive notifications
+                            val curTime = SystemClock.uptimeMillis()
+                            if (curTime - time < 100)
+                                return@progress
+                            else
+                                time = curTime
 
-                    val p: Int = (read * 100 / total).toInt()
-                    nBuilder.setContentText("$p%")
-                            .setProgress(100, p, false)
-                    notifManager.notify(notifID, nBuilder.build())
-                }
-                .responseString { req, res, result ->
-                    val (d, err) = result
-                    nBuilder.setProgress(0, 0, false).setOngoing(false)
-                    notifManager.cancel(notifID)
-                    val notifID = notifID + 1 // FIXME collisions
+                            val p: Int = (read * 100 / total).toInt()
+                            nBuilder.setContentText("$p%")
+                                    .setProgress(100, p, false)
+                            notifManager.notify(notifID, nBuilder.build())
+                        }
+                        .responseString { req, res, result ->
+                            val (d, err) = result
+                            nBuilder.setProgress(0, 0, false).setOngoing(false)
+                            notifManager.cancel(notifID)
+                            val notifID = notifID + 1 // FIXME collisions
 
-                    if(err != null || d == null) {
-                        nBuilder.setContentTitle("Upload failed")
-                                .setContentText(err.toString())
-                        notifManager.notify(notifID, nBuilder.build())
-                    } else {
-                        val url = config.prepareUrl(d)
-                        val i = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                        val intent = PendingIntent.getActivity(this, 0, i, 0)
-                        nBuilder.setContentTitle("Upload successful")
-                                .setContentText(url)
-                                .setContentIntent(intent)
-                        notifManager.notify(notifID, nBuilder.build())
-                    }
-
-                    parent?.setResult(RESULT_OK) ?: setResult(RESULT_OK)
-                    finish()
-                }
-
+                            if (err != null || d == null) {
+                                nBuilder.setContentTitle("Upload failed")
+                                        .setContentText(err.toString())
+                                notifManager.notify(notifID, nBuilder.build())
+                            } else {
+                                val url = config.prepareUrl(d)
+                                val i = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                                val intent = PendingIntent.getActivity(this, 0, i, 0)
+                                nBuilder.setContentTitle("Upload successful")
+                                        .setContentText(url)
+                                        .setContentIntent(intent)
+                                notifManager.notify(notifID, nBuilder.build())
+                            }
+                        }
+            }
+        } catch(e: FileNotFoundException) {
+            Toast.makeText(this, "$uploader not found", Toast.LENGTH_SHORT).show()
+        } finally {
+            parent?.setResult(RESULT_OK) ?: setResult(RESULT_OK)
+            finish()
+        }
     }
 
 }
