4425a43cba93900032827dbed8e3edc8a4738cd8
flan <flan@flande.re>
2017-07-28 20:51:31 +0200

    Validate the uploader before importing

--- app/src/main/java/re/flande/xshare/ImportActivity.kt
+++ app/src/main/java/re/flande/xshare/ImportActivity.kt
@@ -3,6 +3,7 @@ package re.flande.xshare
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.gson.Gson
 import java.io.File
 import java.io.InputStream
 
@@ -27,37 +28,51 @@ class ImportActivity : Activity() {
         if (name.split('.').last() != "sxcu") {
             getFatalDialogBuilder(this)
                     .setMessage(R.string.file_not_sxcu)
-                    .setPositiveButton(R.string.proceed_anyway, { _, _ -> import(name + ".sxcu", in_) })
+                    .setPositiveButton(R.string.proceed_anyway, { _, _ -> import(in_) })
                     .setNegativeButton(android.R.string.cancel, { _, _ -> })
                     .show()
         } else {
-            import(name, in_)
+            import(in_)
         }
     }
 
-    fun import(name: String, inStream: InputStream) {
-        val f = File(getExternalFilesDir(null), name)
+    fun import(inStream: InputStream) {
+        try {
+            val up = makeUploader(inStream)
+            val name = (up.Name + ".sxcu").replace("/", "")
+            val f = File(getExternalFilesDir(null), name)
 
-        if(f.exists()) {
+            if(f.exists()) {
+                getFatalDialogBuilder(this)
+                        .setMessage(resources.getString(R.string.thing_already_exists, up.Name))
+                        .setPositiveButton(R.string.proceed_anyway, { _, _ -> importFile(up, f) })
+                        .setNegativeButton(android.R.string.cancel, { _, _ -> })
+                        .show()
+            } else {
+                importFile(up, f)
+            }
+        } catch (e: Exception) {
             getFatalDialogBuilder(this)
-                    .setMessage(resources.getString(R.string.thing_already_exists, f.name))
-                    .setPositiveButton(R.string.proceed_anyway, { _, _ -> importFile(inStream, f) })
-                    .setNegativeButton(android.R.string.cancel, { _, _ -> })
-                    .show()
-        } else {
-            importFile(inStream, f)
+                    .setTitle(R.string.unable_to_import)
+                    .setMessage(e.message)
+                    .setPositiveButton(android.R.string.ok, { _, _ -> })
         }
     }
 
-    private fun importFile(inStream: InputStream, outFile: File) {
-        // TODO: config validation
+    private fun makeUploader(inStream: InputStream): Uploader {
         inStream.use {
-            outFile.outputStream().use { out ->
-                inStream.copyTo(out)
+            it.reader().use {
+                val uploader = Gson().fromJson(it, Uploader::class.java)
+                uploader.validate()
+                return uploader
             }
         }
+    }
 
-        Toast.makeText(this, resources.getString(R.string.thing_added, outFile.name), Toast.LENGTH_SHORT).show()
+    private fun importFile(up: Uploader, f: File) {
+        Gson().toJson(up, f.writer())
+
+        Toast.makeText(this, resources.getString(R.string.thing_added, up.Name), Toast.LENGTH_SHORT).show()
         finishAffinity()
     }
 }
