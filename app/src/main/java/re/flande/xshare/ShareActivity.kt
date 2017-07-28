a268dc6df1458a5bc5c345596212b1ceaa9a9275
flan <flan@flande.re>
2017-07-28 20:33:47 +0200

    Minor cleanup

--- app/src/main/java/re/flande/xshare/ShareActivity.kt
+++ app/src/main/java/re/flande/xshare/ShareActivity.kt
@@ -3,6 +3,7 @@ package re.flande.xshare
 import android.Manifest
 import android.annotation.TargetApi
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -16,18 +17,20 @@ class ShareActivity : Activity() {
     val REQUESTPERMS_CODE = 0
 
     var uploadCallback: () -> Unit = { throw AssertionError("no upload callback") }
+    lateinit var errDialogBuilder: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        errDialogBuilder = getFatalDialogBuilder(this)
+                .setTitle(R.string.unable_to_upload)
+                .setPositiveButton(android.R.string.ok, { _, _ -> })
 
         // Ideally we would filter out intents without an EXTRA_STREAM in the manifest, but it's
         // not possible. android.intent.category.OPENABLE is said to prevent receiving intents
         // without openable streams, but it doesn't work as intended.
         if (!intent.extras.containsKey(Intent.EXTRA_STREAM)) {
-            getFatalDialogBuilder(this)
-                    .setTitle(R.string.unable_to_upload)
-                    .setMessage(R.string.no_extra_stream)
-                    .setPositiveButton(android.R.string.ok, { _, _ -> })
+            errDialogBuilder.setMessage(R.string.no_extra_stream)
                     .show()
             return
         }
@@ -41,10 +44,7 @@ class ShareActivity : Activity() {
         } else if (intent.action == Intent.ACTION_SEND_MULTIPLE) {
             uris = intent.extras.getParcelableArrayList<Uri>(Intent.EXTRA_STREAM)
         } else {
-            getFatalDialogBuilder(this)
-                    .setTitle(R.string.unable_to_upload)
-                    .setMessage(resources.getString(R.string.intent_action_not_supported, intent.action))
-                    .setPositiveButton(android.R.string.ok, { _, _ -> })
+            errDialogBuilder.setMessage(resources.getString(R.string.intent_action_not_supported, intent.action))
                     .show()
             return
         }
@@ -77,10 +77,7 @@ class ShareActivity : Activity() {
 
         for (res in grantResults) {
             if (res != PackageManager.PERMISSION_GRANTED) {
-                getFatalDialogBuilder(this)
-                        .setTitle(resources.getString(R.string.unable_to_upload))
-                        .setMessage(resources.getString(R.string.no_storage_permission))
-                        .setPositiveButton(android.R.string.ok, { _, _ -> })
+                errDialogBuilder.setMessage(resources.getString(R.string.no_storage_permission))
                         .show()
                 return
             }
